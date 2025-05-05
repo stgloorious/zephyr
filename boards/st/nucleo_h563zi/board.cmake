0d301bf40d8ea25960d4edbb8b1fb4636125730a
Stefan Gloor <stefan.gloor@siemens.com>
2025-05-05 14:11:36 +0200

    boards: st: enable openocd for nucleo_h563zi
    Enable support for OpenOCD for Nucleo H563. This is disabled upstream
    because upstream OpenOCD does not yet support STM32H5xx series.
    We patched our OpenOCD fork to include it.
    
    Signed-off-by: Stefan Gloor <stefan.gloor@siemens.com>

--- boards/st/nucleo_h563zi/board.cmake
+++ boards/st/nucleo_h563zi/board.cmake
@@ -15,5 +15,4 @@ board_runner_args(openocd "--no-halt")
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-#include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-# FIXME: official openocd runner not yet available.
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
